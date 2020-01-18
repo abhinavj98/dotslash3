f803eaab4e240657055657e37b7567c33d288916
Dhruv2012 <dhruv.r.patel14@gmail.com>
2020-01-19 00:54:19 +0530

    FSM left

--- posenet-python/webcam_demo.py
+++ posenet-python/webcam_demo.py
@@ -35,31 +35,54 @@ present_state = STATES["up"]
 MAX = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3}
 MIN = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3}
 DATA = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3}
+'''
+MIN["left_angle"] = 7.97
+MIN["left_speed"] = -3
+MIN["right_angle"] = 9,44
+MIN["right_speed"] = -5.22
+
+MAX["left_angle"] = 123.1
+MAX["left_speed"] = 10.96
+MAX["right_angle"] = 146.7
+MAX["right_speed"] = 0
+'''
+
+def init_param():
+    MIN["left_angle"] = 1000
+    MIN["left_speed"] = 1000
+    MIN["right_angle"] = 1000
+    MIN["right_speed"] = 1000
+
+    MAX["left_angle"] = 0
+    MAX["left_speed"] = 0
+    MAX["right_angle"] = 0
+    MAX["right_speed"] = 0
+
 def update_max():
     if(MAX["left_angle"] <= DATA["left_angle"]):
-        MAX["left_angle"] = DATA[["left_angle"]]
+        MAX["left_angle"] = DATA["left_angle"]
 
     if(MAX["right_angle"] <= DATA["right_angle"]):
-        MAX["right_angle"] = DATA[["right_angle"]]
+        MAX["right_angle"] = DATA["right_angle"]
 
     if(MAX["left_speed"] <= DATA["left_speed"]):
-        MAX["left_speed"] = DATA[["left_speed"]]
+        MAX["left_speed"] = DATA["left_speed"]
     
     if(MAX["right_speed"] <= DATA["right_speed"]):
-        MAX["right_speed"] = DATA[["right_speed"]]
+        MAX["right_speed"] = DATA["right_speed"]
 
 def update_min():
     if(MIN["left_angle"] >= DATA["left_angle"]):
-        MIN["left_angle"] = DATA[["left_angle"]]
+        MIN["left_angle"] = DATA["left_angle"]
 
     if(MIN["right_angle"] >= DATA["right_angle"]):
-        MIN["right_angle"] = DATA[["right_angle"]]
+        MIN["right_angle"] = DATA["right_angle"]
 
     if(MIN["left_speed"] >= DATA["left_speed"]):
-        MIN["left_speed"] = DATA[["left_speed"]]
+        MIN["left_speed"] = DATA["left_speed"]
     
     if(MIN["right_speed"] >= DATA["right_speed"]):
-        MIN["right_speed"] = DATA[["right_speed"]]
+        MIN["right_speed"] = DATA["right_speed"]
     
 def get_angle(arr,num):
     dist1 = 0
@@ -92,6 +115,7 @@ def get_angle(arr,num):
     dist2 = math.sqrt((x2-x3)**2 + (y2-y3)**2)
     dist3 = math.sqrt((x1-x3)**2 + (y1-y3)**2)
     cosine = (dist1**2 + dist2**2 - dist3**2)/(2*dist1*dist2)
+
     return math.acos(cosine)*(180/math.pi) 
 
 
@@ -116,7 +140,7 @@ def main():
         # else:
         #     cap = cv2.VideoCapture(args.cam_id)
 
-        cap = cv2.VideoCapture("/home/dhruv/Downloads/side.mp4")
+        cap = cv2.VideoCapture("/home/dhruv/DOTSLASH/dotslash3/posenet-python/side_v1.mp4")
         cap.set(3, args.cam_width)
         cap.set(4, args.cam_height)
 
@@ -128,6 +152,7 @@ def main():
         movingarr_index = 0
         movingarr_left_angle = np.zeros(movingarr_size)
         movingarr_right_angle = np.zeros(movingarr_size)
+        init_param()
         while True:
             start_iter = time.time()
             input_image, display_image, output_scale = posenet.read_cap(
@@ -159,6 +184,8 @@ def main():
             DATA["left_angle"] = lowpass(left_angle,movingarr_left_angle,movingarr_index,movingarr_size)
             DATA["right_angle"] = lowpass(right_angle,movingarr_right_angle,movingarr_index,movingarr_size)
             movingarr_index+=1
+            update_max()
+            update_min()
             #print("langle:")
             #print(opt_left_angle)
             #print("rangle:")
@@ -178,12 +205,21 @@ def main():
             #print("right_speed:")
             #print(right_speed)
             
-            prev_left_angle = DATA["left_speed"]
-            prev_right_angle = DATA["right_speed"]
+            prev_left_angle = DATA["left_angle"]
+            prev_right_angle = DATA["right_angle"]
+            print(MAX)
+            print(MIN)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        print('Average FPS: ', frame_count / (time.time() - start))
+            switch(present_state){
+                case STATES["up"]:
+                    if(DATA["left"])
 
+
+            }
+        print('Average FPS: ', frame_count / (time.time() - start))
+        print(MAX)
+        print(MIN)
 
 if __name__ == "__main__":
     main()
