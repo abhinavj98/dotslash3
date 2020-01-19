41811eb6e29725b5836f06576ae558555613ec41
Dhruv2012 <dhruv.r.patel14@gmail.com>
2020-01-19 13:32:38 +0530

    Working kinda

---
+++ backend/constants.py
@@ -0,0 +1,13 @@
+PART_NAMES = {
+    "nose":0, "leftEye":1, "rightEye":2, "leftEar":3, "rightEar":4, "leftShoulder":5,
+    "rightShoulder":6, "leftElbow":7, "rightElbow":8, "leftWrist":9, "rightWrist":10,
+    "leftHip":11, "rightHip":12, "leftKnee":13, "rightKnee":14, "leftAnkle":15, "rightAnkle":16
+}
+STATES = {"pause":0, "up":1, "down":2}
+present_state = {"left_arm":0, "right_arm":1}
+MAX = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3}
+MIN = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3}
+NORM = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3}
+DATA = {"left_angle":0, "right_angle":1, "left_speed":2, "right_speed":3, "avg_left_speed":4, "avg_right_speed":5}
+temp1 =0
+temp2 =0
